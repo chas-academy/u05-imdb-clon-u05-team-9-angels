6be3010104d32910530fe5a5d421497d0abedefb
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2021-02-10 20:42:14 +0100

    Add seeder for actors and movies

---
+++ database/factories/MovieFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Movie;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MovieFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Movie::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->text(25),
+            'description' => $this->faker->text(100),
+            'year' => $this->faker->year,
+        ];
+    }
+}
