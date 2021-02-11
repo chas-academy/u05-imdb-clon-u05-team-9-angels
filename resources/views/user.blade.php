77f62a9f51596a0d3b7ba40d1f716530ff2e77eb
linusunghammar <unghammarlinus@gmail.com>
2021-02-11 14:53:59 +0100

    add picture section, for profile

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{ $user->name }}</h1>
+<div class="container mx-auto px-4 pt-16">
+    <h1 class="tracking-wider text-gray-600 text-lg font-semibold">Welcome back {{ $user->name }}!</h1>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-8">
+        {{-- Profile picture section, just a picture of chuck norris atm --}}
+        <div class="mt-8">  
+                <img src= "https://m.media-amazon.com/images/M/MV5BMTU4NjY3NzgyM15BMl5BanBnXkFtZTcwODI4OTEzNA@@._V1_UY317_CR18,0,214,317_AL_.jpg" alt="Poster">
+        </div>
+        {{-- Profile picture ends --}}
+    </div>
+</div>
 @endsection
