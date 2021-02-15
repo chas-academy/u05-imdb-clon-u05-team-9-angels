b1167742f8c30eea1b8be7b7a2527da5a74a38d3
linusunghammar <unghammarlinus@gmail.com>
2021-02-15 09:57:59 +0100

    add buttons

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,7 +4,7 @@
 <main class="container mx-auto px-4 pt-16">
     <h1 class="text-lg font-bold">Welcome back {{ $user->name }}!</h1>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-8">
-        {{-- Profile info section, just a picture of chuck norris atm --}}
+        {{-- Profile info section, just a random picture atm --}}
         <div class="mt-8 mb-12">  
                 <img src= "https://m.media-amazon.com/images/M/MV5BMTU4NjY3NzgyM15BMl5BanBnXkFtZTcwODI4OTEzNA@@._V1_UY317_CR18,0,214,317_AL_.jpg" alt="Poster">
                 <button
@@ -30,9 +30,15 @@
             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
             when an unknown printer took a galley of type and scrambled it to make a type
             specimen book. It has survived not only five centuries,</p>
+            <button
+                type="button"
+                class="border border-gray-200 bg-gray-200 text-gray-700 rounded-3xl px-4 py-2 transition 
+                duration-500 ease select-none hover:bg-gray-300 focus:outline-none focus:shadow-outline mt-5">
+                See more...
+            </button>
         </div>
-    {{-- your reviews section ends--}}
-    {{-- your whatchlist section--}}
+        {{-- your reviews section ends--}}
+        {{-- your whatchlist section--}}
         <div class="border p-10 border-gray-800 rounded-lg mb-10">
             <h3 class="text-lg font-bold mb-6">Your Watchlist</h3>
             <h4 class="font-bold">Interstellar</h4>
@@ -45,10 +51,16 @@
             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
             when an unknown printer took a galley of type and scrambled it to make a type
             specimen book. It has survived not only five centuries,</p>
+            <button
+                type="button"
+                class="border border-gray-200 bg-gray-200 text-gray-700 rounded-3xl px-4 py-2 transition 
+                duration-500 ease select-none hover:bg-gray-300 focus:outline-none focus:shadow-outline mt-5">
+                See more...
+            </button>
         </div>
     </section>
     {{-- your whatchlist section ends--}}
-   {{-- Recently reviewed section --}}
+    {{-- Recently reviewed section --}}
    <section class="border p-10 border-gray-800 rounded-lg mb-10">
         <h3 class="text-lg font-semibold">Recently viewed movies</h3>
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
