f3901529022fdbd6c5ed2047156ad0e1112cbb93
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2021-02-15 13:00:46 +0100

    Update actor controller
    Basic CRUD functionality

--- app/Http/Controllers/ActorController.php
+++ app/Http/Controllers/ActorController.php
@@ -7,16 +7,106 @@ use Illuminate\Http\Request;
 
 class ActorController extends Controller
 {
-    public function getAll()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
     {
         $actors = Actor::all();
-        return view('actor', ['actors' => $actors]);
+        return view('actor.index')->with('actors', $actors);
     }
-    public function index($id)
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
     {
-        $actors = Actor::where('id', $id)->first();
-        return view('actor', [
-            'actor' => $actors,
+        return view('actor.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'bail|required|string|max:255',
+            'age' => 'bail|required|numeric|between:0,150',
+            'description' => 'bail|required|string|max:255'
         ]);
+
+        $actor = Actor::create([
+            'name' => $request->input('name'),
+            'age' => $request->input('age'),
+            'description' => $request->input('description'),
+        ]);
+
+        return $actor->save() ? 'Actor saved' : 'Error saving actor';
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Actor  $actor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Actor $actor)
+    {
+        $dummyCreditsMovie = [
+            'title' => 'Movie Title',
+            'character' => 'Character/Role',
+        ];
+        return view('actor.show')->with('actor', $actor)->with('creditsMovie', $dummyCreditsMovie);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Actor  $actor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Actor $actor)
+    {
+        return view('actor.edit')->with('actor', $actor);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Actor  $actor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Actor $actor)
+    {
+        $validated = $request->validate([
+            'name' => 'bail|required|string|max:255',
+            'age' => 'bail|required|numeric|between:0,150',
+            'description' => 'bail|required|string|max:255'
+        ]);
+
+        return $actor->update([
+            'name' => $request->input('name'),
+            'age' => $request->input('age'),
+            'description' => $request->input('description'),
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Actor  $actor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Actor $actor)
+    {
+        return $actor->delete();
     }
 }
