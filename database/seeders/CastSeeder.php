ade589a00ebbb3f09d9bd7c3b9c54c05e60ac253
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2021-02-15 18:09:59 +0100

    Add cast seeder

---
+++ database/seeders/CastSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Cast;
+use Illuminate\Database\Seeder;
+
+class CastSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Cast::factory()
+                ->count(50)
+                ->create();
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             MovieSeeder::class,
             ActorSeeder::class,
+            CastSeeder::class,
         ]);
     }
 }
