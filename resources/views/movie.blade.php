85bc03599599e94826d4252ec13900697ff05848
frankemon <frankie.hugo@hotmail.com>
2021-02-18 17:21:00 +0100

    movie-page add cast styling rework

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -17,20 +17,13 @@
     
     <div class="border-t border-gray-200">
       <dl>
-        {{-- <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-500">
-          </dt>
-          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-        <h1 class="text-lg font-bold">{{$movies->title}} ({{$movies->year}})</h1>
-          </dd>
-        </div> --}}
-        <img src= "https://m.media-amazon.com/images/M/MV5BMTU4NjY3NzgyM15BMl5BanBnXkFtZTcwODI4OTEzNA@@._V1_UY317_CR18,0,214,317_AL_.jpg" alt="Poster">
+       <img src= "https://m.media-amazon.com/images/M/MV5BMTU4NjY3NzgyM15BMl5BanBnXkFtZTcwODI4OTEzNA@@._V1_UY317_CR18,0,214,317_AL_.jpg" alt="Poster">
         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-8 sm:gap-4 sm:px-6">
           <dt class="text-sm font-medium text-gray-500 font-bold">
             Director
           </dt>
+          Samwise Gamgee // needs to be dynamic
           <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-            Samwise Gamgee // needs to be dynamic
           </dd>
         </div>
         <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-8 sm:gap-4 sm:px-6">
@@ -72,53 +65,44 @@
   {{-- end movie card --}}
 
         <h2 class="text-2xl mt-10 mb-10"><span class="font-bold">Description:</span> {{$movies->description}}</h2>
-
         <h3 class="text-2xl  font-bold mb-5">Cast: </h3>
-        <ul>
-        @if (isset($actor_list))
-            @foreach($actor_list as $actor_var)
-                @foreach ($actor_var as $print)
-                    <li>
-                        <div class="grid p-5">
-                            <div>
-                            <section class="grid grid-cols-1 sm:grid-cols-6 gap-4">
-                            {{-- <section class="grid grid-cols-1 sm:grid-cols-6 gap-4"> --}}
-                                <!-- CARD 1 -->
-                                <div class="bg-gray-900 shadow-lg rounded p-3">
-                                <div class="group relative">
-                                    <img src="https://m.media-amazon.com/images/M/MV5BMTU4NjY3NzgyM15BMl5BanBnXkFtZTcwODI4OTEzNA@@._V1_UY317_CR18,0,214,317_AL_.jpg" alt="" />
-                                </div>
-                                <div class="p-5">
-                                    <h3 class="text-white text-lg">{{$print->name}}</h3>
-                                    <p class="text-gray-400">Character name</p>
-                                </div>
-                                </div>
-                                <!-- END OF CARD 1 -->
-                        
-                            </section>
-                            </div>
-
-
-
-
-
-
-                        {{-- <h4 class="font-bold">{{$print->name}}</h4>
-                        <p>For later: Role</p>
-                        <img src= "https://m.media-amazon.com/images/M/MV5BMTU4NjY3NzgyM15BMl5BanBnXkFtZTcwODI4OTEzNA@@._V1_UY317_CR18,0,214,317_AL_.jpg" alt="Poster"> --}}
-
-                    </li>
-                @endforeach
-            @endforeach
+          <div class="container mx-auto px-4 pt-16">
+            <div class="cast-list">
+              <div class="grid grid-cols-2 gap-8">
+                 @if (isset($actor_list))
+                  @foreach($actor_list as $actor_var)
+                      @foreach ($actor_var as $print)
+                      {{-- start cast card --}}
+                      {{-- <div class="mt-8 text-center max-w-xs bg-gray-500" style="max-height:250px">  --}}
+                        <div class="bg-gray-300 p-2 sm:rounded-lg" style="max-width:250px">
+                        <a href="#">
+                          <img class="sm:rounded-lg w-full" src= "https://m.media-amazon.com/images/M/MV5BMTU4NjY3NzgyM15BMl5BanBnXkFtZTcwODI4OTEzNA@@._V1_UY317_CR18,0,214,317_AL_.jpg" alt="Poster">
+                        </a>
+                        <div class="mt-2">
+                          <a href="#" class="text-lg mt-2 text-black">{{$print->name}}</a>
+                          <div class="text-gray-800">
+                            <p>Character name</p>
+                          </div>
+                          </div>
+                        </div>
+                      {{-- </div> --}}
+                      @endforeach
+                      @endforeach
+              </div>
+            </div> 
+          </div> 
+             
         @else 
             <h4>Cast not available</h4>
         @endif
-    </ul>
     @else
         <p>Movie does not exist</p>
         <a href="/">Back to start</a>
     @endif
-   
+
+
+  {{-- end of cast container --}}
+  
 
        
 
