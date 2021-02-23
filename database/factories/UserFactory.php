7e0c9bf74dd6f769313d9f23db4281f02e34e9b1
Erik <kireSuomi@gmail.com>
2021-02-23 14:54:27 +0100

    User seeder + Factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,6 +28,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'type' => $this->faker->numberBetween(0, 3)
         ];
     }
 }
