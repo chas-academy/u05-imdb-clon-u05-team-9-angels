3ad936e6c5715f9ee005a0fd9ce90041be32cf89
Oskar Findahl <oskar_findahl@hotmail.com>
2021-02-26 18:52:01 +0100

    Navbar and movies with pictures, title and year

--- resources/views/movies-all.blade.php
+++ resources/views/movies-all.blade.php
@@ -1,21 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+
+
+@section('content')
 
 <body>
+
     <h1>Movie page</h1>
-    {{-- <h2>Title: {{$movies->title}}</h2> --}}
+    
     @foreach ($movies as $movie)
-    <p>{{$movie}}</p>
+    
+    {{-- card for movie --}}
+                <div class="container mx-auto px-4 pt-16 mb-10" style="" >
+                <h3>{{$movie->title}}</h3>
+                    <img class="mb-10"
+                        src="https://m.media-amazon.com/images/M/MV5BMTU4NjY3NzgyM15BMl5BanBnXkFtZTcwODI4OTEzNA@@._V1_UY317_CR18,0,214,317_AL_.jpg"
+                        alt="Poster">
+                   
+                    <p><span class="font-bold">Released:</span> {{$movie->year}}</p>
+                   
+                    
+                </div>
+                <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+                    {{-- end movie card --}}
+
     @endforeach
 
     
 </body>
 
-</html>
+@endsection
