810fe6c61b84fcd0c51a66767197f004ee0895a3
linusunghammar <unghammarlinus@gmail.com>
2021-03-03 15:21:10 +0100

    fetch data from authusercontroller instead

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,17 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
 
-    public function index($id)
+    public function index()
     {
-        $user = User::where('id', $id)->first();
-        return view('user', [
-            'user' => $user,
-        ]);
     }
 }
