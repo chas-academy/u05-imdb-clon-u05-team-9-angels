433cc09eb8546b5e5734325c997407f484ebe19c
Oskar Findahl <oskar_findahl@hotmail.com>
2021-03-07 15:32:43 +0100

    Seeding from TMDB API

--- database/seeders/MovieSeeder.php
+++ database/seeders/MovieSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\Movie;
 use Illuminate\Database\Seeder;
 
+use Illuminate\Support\Facades\Http;
+
 class MovieSeeder extends Seeder
 {
     /**
@@ -14,8 +16,21 @@ class MovieSeeder extends Seeder
      */
     public function run()
     {
+        for ($x = 100; $x <= 110; $x++){
+        $data = Http::get("https://api.themoviedb.org/3/movie/{$x}?api_key=df7b9ec54824bdaded1b2ad9585f13a4")->json();
         Movie::factory()
-                ->count(25)
-                ->create();
+        ->count(1)
+        ->create([
+            'title' => $data['original_title'],
+            'description' => $data['overview'],
+            'year' => $data['release_date'],
+        ]);
+         
+}
+        // Movie::factory()
+        //         ->count(1)
+        //         ->create();
+
     }
+    
 }
