e0795d4afbd7c33d4773e6aaeec6751f0a51d7d8
ichbinjaeger <ichbinjaeger@users.noreply.github.com>
2021-03-07 17:37:22 +0100

    add styling to actor index

--- resources/views/actor/index.blade.php
+++ resources/views/actor/index.blade.php
@@ -3,12 +3,35 @@
 @section('title', 'Actors')
 
 @section('content')
-    <div>
+    {{-- <div>
         <h1>All actors</h1>
         <ul>
             @foreach ($actors as $actor)
                 <li>{{ $actor }}</li>
             @endforeach
         </ul>
+    </div> --}}
+
+
+    <div class="container mx-auto px-4 pt-16">
+        <h2 class="tracking-wider text-2xl font-semibold">Actors page</h2>
+        {{-- Movie section --}}
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8 mb-5">
+            @foreach ($actors as $actor)
+                <div class="mt-8">
+                    <a href="{{ url('actors/' . $actor->id) }}">
+                        <img src="https://m.media-amazon.com/images/M/MV5BMTU4NjY3NzgyM15BMl5BanBnXkFtZTcwODI4OTEzNA@@._V1_UY317_CR18,0,214,317_AL_.jpg"
+                            alt="Poster" class="hover:opacity-75 transition
+                                                        ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="{{ url('actors/' . $actor->id) }}"
+                            class="text-lg mt-2 hover:text-gray:300">{{ $actor->name }}</a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+        {{-- Movie section ends --}}
     </div>
+
 @endsection
