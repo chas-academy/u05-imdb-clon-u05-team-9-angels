40d98193a8ab1dbebf643bcf57fbfc3af1cb585e
linusunghammar <unghammarlinus@gmail.com>
2021-03-13 12:17:20 +0100

    adds link from known movies

--- resources/views/components/actor/movie-group.blade.php
+++ resources/views/components/actor/movie-group.blade.php
@@ -1,8 +1,10 @@
 <div class="bg-gray-200 pt-2 rounded-lg shadow-md w-full">
     <p class="pl-4 text-2xl font-semibold">{{ $title }}</p>
-    <div class="flex flex-nowrap overflow-x-auto overflow-y-hidden pl-4 pb-2 mt-2 space-x-4" >
+    <div class="flex flex-nowrap overflow-x-auto overflow-y-hidden pl-4 pb-2 mt-2 space-x-4">
         @foreach ($movies as $movie)
+        <a href="{{ url('movies/'.$movie->id) }}">
             <x-actor.movie-thumbnail :title="$movie['title']" :year="$movie['year']" />
+        </a>
         @endforeach
     </div>
 </div>
