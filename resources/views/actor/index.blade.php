014a2240ca3691bbc65c37260e678c33bbe3ff92
ichbinjaeger <ichbinjaeger@users.noreply.github.com>
2021-03-14 19:05:25 +0100

    add modal to actor

--- resources/views/actor/index.blade.php
+++ resources/views/actor/index.blade.php
@@ -15,14 +15,13 @@
 
     <div class="container mx-auto px-4 pt-16">
         <h2 class="tracking-wider text-2xl font-semibold">Actors page</h2>
-        {{-- Movie section --}}
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8 mb-5">
             @foreach ($actors as $actor)
                 <div class="mt-8">
                     <a href="{{ url('actors/' . $actor->id) }}">
                         <img src="https://m.media-amazon.com/images/M/MV5BMTU4NjY3NzgyM15BMl5BanBnXkFtZTcwODI4OTEzNA@@._V1_UY317_CR18,0,214,317_AL_.jpg"
                             alt="Poster" class="hover:opacity-75 transition
-                                                        ease-in-out duration-150">
+                                                            ease-in-out duration-150">
                     </a>
                     <div class="mt-2">
                         <a href="{{ url('actors/' . $actor->id) }}"
@@ -31,7 +30,6 @@
                 </div>
             @endforeach
         </div>
-        {{-- Movie section ends --}}
     </div>
 
 @endsection
--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -25,8 +25,8 @@
                                     <g data-name="Layer 2">
                                         <path
                                             d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 
-                                                                                                                                                                                                                                                                                                                                                                                                                        1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 
-                                                                                                                                                                                                                                                                                                                                                                                                                        01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z"
+                                                                                                                                                                                                                                                                                                                                                                                                                            1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 
+                                                                                                                                                                                                                                                                                                                                                                                                                            01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z"
                                             data-name="star"></path>
                                     </g>
                                 </svg>
@@ -89,18 +89,6 @@
     @if ($can_edit)
         <div class="form-container mx-14">
             <!-- EDIT MODAL -->
-            {{-- <style>
-                .modal {
-                    transition: opacity 0.25s ease;
-                }
-
-                body.modal-active {
-                    overflow-x: hidden;
-                    overflow-y: visible !important;
-                }
-
-            </style> --}}
-            {{-- <link rel="stylesheet" href="{{ asset('css/modal.css') }}"> --}}
 
             <body class="bg-gray-200 flex items-center justify-center h-screen">
 
