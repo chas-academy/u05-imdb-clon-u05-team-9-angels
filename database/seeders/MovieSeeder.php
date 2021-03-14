44ad498aa89343cad49145ac6dc778f3094ef732
ichbinjaeger <ichbinjaeger@users.noreply.github.com>
2021-03-14 18:21:21 +0100

    add rest of crud for movie, add modal and js (in public/js)

--- database/seeders/MovieSeeder.php
+++ database/seeders/MovieSeeder.php
@@ -16,36 +16,27 @@ class MovieSeeder extends Seeder
      */
     public function run()
     {
-        for ($x = 1; $x <= 80; $x++){
-        $data = Http::get("https://api.themoviedb.org/3/movie/{$x}?api_key=df7b9ec54824bdaded1b2ad9585f13a4")->json();
-       
-     if(isset($data['title']) && isset($data['overview']) && isset($data['release_date']) && isset($data['poster_path'])){
+        for ($x = 1; $x <= 80; $x++) {
+            $data = Http::get("https://api.themoviedb.org/3/movie/{$x}?api_key=df7b9ec54824bdaded1b2ad9585f13a4")->json();
 
-        Movie::factory()
-        ->count(1)
-        ->create([
-            'title' =>$data['title'],
-            'description' => $data['overview'],
-            'year' => $data['release_date'],
-            'runtime' => $data['runtime'],
-            'rating' => $data['vote_average'],
-            'poster' => $data['poster_path'],
-            
-            
-        ]);
+            if (isset($data['title']) && isset($data['overview']) && isset($data['release_date']) && isset($data['poster_path'])) {
 
+                Movie::factory()
+                    ->count(1)
+                    ->create([
+                        'title' => $data['title'],
+                        'description' => $data['overview'],
+                        'year' => $data['release_date'],
+                        'runtime' => $data['runtime'],
+                        'rating' => $data['vote_average'],
+                        'poster' => $data['poster_path'],
 
+                    ]);
+            }
         }
-        
-       
-
-
-         
-}
         // Movie::factory()
         //         ->count(1)
         //         ->create();
 
     }
-    
 }
