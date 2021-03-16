c1404eb36a1b119c98c9ab943c05974a40d1f79e
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2021-03-16 12:38:14 +0100

    Add movie posters in actor page
    Replace placeholder posters with external source from TMDb.

---
+++ resources/views/components/actor/actor-poster.blade.php
@@ -0,0 +1,8 @@
+<div class="mt-8">
+    <a href="{{ $url }}">
+        <img src="{{ $imageUrl }}" alt="Poster" class="hover:opacity-75 transition ease-in-out duration-150">
+    </a>
+    <div class="mt-2">
+        <a href="{{ $url }}" class="text-lg mt-2 hover:text-gray:300">{{ $name }}</a>
+    </div>
+</div>
--- resources/views/components/actor/movie-group.blade.php
+++ resources/views/components/actor/movie-group.blade.php
@@ -1,10 +1,11 @@
 <div class="bg-gray-200 pt-2 rounded-lg shadow-md w-full">
     <p class="pl-4 text-2xl font-semibold">{{ $title }}</p>
-    <div class="flex flex-nowrap overflow-x-auto overflow-y-hidden pl-4 pb-2 mt-2 space-x-4">
+    <div class="flex flex-nowrap overflow-x-auto pl-4 pb-2 mt-2 space-x-4">
         @foreach ($movies as $movie)
-        <a href="{{ url('movies/'.$movie->id) }}">
-            <x-actor.movie-thumbnail :title="$movie['title']" :year="$movie['year']" />
-        </a>
+            <a href="{{ url('movies/' . $movie->id) }}">
+                <x-actor.movie-thumbnail imageUrl="https://image.tmdb.org/t/p/w154/{{ $movie->poster }}"
+                    :title="$movie['title']" :year="$movie['year']" />
+            </a>
         @endforeach
     </div>
 </div>
