5f45b74e12e0b000161000fef784f1006a32d582
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2021-03-28 21:38:15 +0200

    Fix style spacing issues

--- resources/views/components/actor/movie-group.blade.php
+++ resources/views/components/actor/movie-group.blade.php
@@ -1,21 +1,25 @@
 <div class="bg-imdb-card pt-2 rounded-lg shadow-md w-full">
     <p class="pl-4 text-2xl font-semibold">{{ $title }}</p>
     @if ($isCarousel)
-        <div class="flex flex-nowrap overflow-x-auto gap-x-4">
+        <div class="flex flex-nowrap overflow-x-auto gap-x-4 px-4">
             @foreach ($movies as $movie)
-                <a href="{{ url('movies/' . $movie->id) }}">
-                    <x-actor.movie-thumbnail imageUrl="https://image.tmdb.org/t/p/w154/{{ $movie->poster }}"
-                        :title="$movie->title" :year="$movie->year" :id="$movie->id" />
-                </a>
+                <div class="flex-shrink-0">
+                    <a href="{{ url('movies/' . $movie->id) }}">
+                        <x-actor.movie-thumbnail imageUrl="https://image.tmdb.org/t/p/w154/{{ $movie->poster }}"
+                            :title="$movie->title" :year="$movie->year" :id="$movie->id" />
+                    </a>
+                </div>
             @endforeach
         </div>
     @else
-        <div class="flex flex-row flex-wrap gap-x-2 xl:gap-x-12">
+        <div class="flex flex-row flex-wrap gap-x-4 md:gap-x-8 px-4">
             @foreach ($movies as $movie)
-                <a href="{{ url('movies/' . $movie->id) }}">
-                    <x-actor.movie-thumbnail imageUrl="https://image.tmdb.org/t/p/w154/{{ $movie->poster }}"
-                        :title="$movie->title" :year="$movie->year" :id="$movie->id" />
-                </a>
+                <div>
+                    <a href="{{ url('movies/' . $movie->id) }}">
+                        <x-actor.movie-thumbnail imageUrl="https://image.tmdb.org/t/p/w154/{{ $movie->poster }}"
+                            :title="$movie->title" :year="$movie->year" :id="$movie->id" />
+                    </a>
+                </div>
             @endforeach
         </div>
     @endif
--- resources/views/components/actor/movie-thumbnail.blade.php
+++ resources/views/components/actor/movie-thumbnail.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col gap-y-2 flex-shrink sm:flex-shrink-0">
+<div class="flex flex-col gap-y-2">
     <a href="{{ url('movies/' . $id) }}">
         <img src="{{ $imageUrl }}"
             class="rounded-lg shadow-md hover:opacity-75 transition ease-in-out duration-150 w-full"
@@ -6,7 +6,7 @@
     </a>
     <div class="mt-auto">
         <a href="{{ url('movies/' . $id) }}" class="hover:text-gray-300 transition ease-in-out duration-150">
-            <p class="leading-4 overflow-ellipsis overflow-hidden">{{ $title }}</p>
+            <p class="leading-4 font-semibold">{{ $title }}</p>
             <p>{{ $year }}</p>
         </a>
     </div>
