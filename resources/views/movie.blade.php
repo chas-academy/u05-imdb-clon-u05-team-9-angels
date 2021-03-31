37ebce52e4eb78ae25bf8a0380f12e8028b880d1
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2021-03-31 23:26:33 +0200

    Update movie page

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -15,8 +15,8 @@
                             <g data-name="Layer 2">
                                 <path
                                     d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1
-                                                                                                                    1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0
-                                                                                                                    01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z"
+                                                                                                                                        1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0
+                                                                                                                                        01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z"
                                     data-name="star"></path>
                             </g>
                         </svg>
@@ -25,13 +25,12 @@
                         @if (Auth::check())
                             @if ($watchlist)
                                 <button><a href="{{ url('/movies/remove-from-watchlist/' . $watchlistId) }}"
-                                        class="border btn border-indigo-500 bg-red-500 text-white rounded-md px-4 py-2 transition duration-500 ease select-none hover:bg-red-600 focus:outline-none focus:shadow-outline">Remove
+                                        class="btn-red">Remove
                                         from list</a>
                                 </button>
                             @else
                                 <button>
-                                    <a href="{{ url('/movies/add-to-watchlist/' . $movies->id) }}"
-                                        class="border border-indigo-500 bg-green-500 text-white rounded-md px-4 py-2 transition duration-500 ease select-none hover:bg-green-600 focus:outline-none focus:shadow-outline">
+                                    <a href="{{ url('/movies/add-to-watchlist/' . $movies->id) }}" class="btn-green">
                                         Add to list
                                     </a>
                                 </button>
@@ -44,9 +43,11 @@
                     <p><span class="font-bold">Runtime:</span> {{ $movies->runtime }} min</p>
                     <p class="pb-2"><span class="font-bold">Genre:</span> {{ $movies->genre }}</p>
                     @if ($can_edit)
-                        <button
-                            class="modal-open mb-5 border border-indigo-500 bg-indigo-500 text-white rounded-md px-4 py-2 mt-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none      focus:shadow-outline">
+                        <button class="modal-open mb-5 btn-blue">
                             Edit movie
+                        </button>
+                        <button class="delete-modal-open btn-red">
+                            Delete Movie
                         </button>
                     @endif
                 </div>
@@ -146,8 +147,8 @@
                                                     <g data-name="Layer 2">
                                                         <path
                                                             d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1
-                                                                                                                            1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0
-                                                                                                                            01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z"
+                                                            1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0
+                                                            01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z"
                                                             data-name="star"></path>
                                                     </g>
                                                 </svg>
@@ -183,8 +184,8 @@
                                                 <g data-name="Layer 2">
                                                     <path
                                                         d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1
-                                                                                                                            1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0
-                                                                                                                            01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z"
+                                                        1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0
+                                                        01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z"
                                                         data-name="star"></path>
                                                 </g>
                                             </svg>
@@ -254,47 +255,39 @@
 
                                     <label for="title">Title:</label><br>
                                     <input type="text" id="title" name="title" value="{{ $movies->title }}" required><br>
-                                    <input type="hidden" id="title" name="id" value="{{ $movies->id }}">
                                     <br>
 
                                     <label for="description">Description:</label><br>
                                     <input type="text" id="description" name="description"
                                         value="{{ $movies->description }}" required><br>
-                                    <input type="hidden" id="description" name="id" value="{{ $movies->id }}">
                                     <br>
 
                                     <label for="rating">Rating:</label><br>
                                     <input type="number" id="rating" name="rating" value="{{ $movies->rating }}" min="0"
                                         required><br>
-                                    <input type="hidden" id="rating" name="id" value="{{ $movies->id }}">
                                     <br>
 
                                     <label for="director">Director:</label><br>
                                     <input type="text" id="director" name="director" value="{{ $movies->director }}"
                                         required><br>
-                                    <input type="hidden" id="director" name="id" value="{{ $movies->id }}">
                                     <br>
 
                                     <label for="writer">Writer:</label><br>
                                     <input type="text" id="writer" name="writer" value="{{ $movies->writer }}"
                                         required><br>
-                                    <input type="hidden" id="writer" name="id" value="{{ $movies->id }}">
                                     <br>
 
                                     <label for="year">Released:</label><br>
                                     <input type="date" id="year" name="year" value="{{ $movies->year }}" required><br>
-                                    <input type="hidden" id="year" name="id" value="{{ $movies->id }}">
                                     <br>
 
                                     <label for="genre">Genre:</label><br>
                                     <input type="text" id="genre" name="genre" value="{{ $movies->genre }}" required><br>
-                                    <input type="hidden" id="genre" name="id" value="{{ $movies->id }}">
                                     <br>
 
                                     <label for="runtime">Runtime (minutes):</label><br>
                                     <input type="number" id="runtime" name="runtime" value="{{ $movies->runtime }}"
-                                        min="0" required><br>
-                                    <input type="hidden" id="runtime" name="id" value="{{ $movies->id }}">
+                                        min="0" required>
                                     <br>
                                     <div class="flex justify-end pt-2">
 
@@ -309,7 +302,50 @@
                             </div>
                         </div>
                     </div>
+                    {{-- delete modal --}}
+                    <div
+                        class="delete-modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center">
+                        <div class="delete-modal-open absolute w-full h-full bg-gray-900 opacity-50"></div>
+                        <div
+                            class="modal-container bg-imdb-black w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+
+                            <div
+                                class="delete-modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
+                                <svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18"
+                                    height="18" viewBox="0 0 18 18">
+                                    <path
+                                        d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z">
+                                    </path>
+                                </svg>
+                                <span class="text-sm">(Esc)</span>
+                            </div>
+
+                            <div class="modal-content py-4 text-left px-6">
+                                <!--Title-->
+                                <div class="flex justify-between items-center pb-3">
+                                    <h3 class="text-2xl font-bold text-white">Are you sure you want to delete
+                                        {{ $movies->title }}?</h3>
+                                    <div class="delete-modal-close cursor-pointer z-50">
+                                        <svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18"
+                                            height="18" viewBox="0 0 18 18">
+                                            <path
+                                                d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z">
+                                            </path>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <form method="POST" action="/movies/delete/{{ $movies->id }}">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button class="btn-red" type="submit">Delete</button>
+                                    <button class="delete-modal-close btn-green">Close</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- delete model ends --}}
                     <script src="{{ asset('js/modal.js') }}"></script>
+                    <script src="{{ asset('js/delete-modal.js') }}"></script>
                     {{-- END OF EDIT-MODAL --}}
                 </div>
             @endif
